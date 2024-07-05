1c92fe1fe99d5e9cfb890bdc423cdac76b7d9e89
Odenir Gomes <odenirdev@gmail.com>
2024-07-04 22:18:56 -0300

    feat: reordenando como trabalhamos

--- src/components/templates/HowWeWork.tsx
+++ src/components/templates/HowWeWork.tsx
@@ -46,10 +46,8 @@ export const HowWeWork = () => {
         "#methodology > div",
         {
           opacity: 0,
-          x: -25,
         },
         {
-          x: 0,
           opacity: 1,
           ease: "power1.in",
           duration: 0.4,
@@ -80,62 +78,58 @@ export const HowWeWork = () => {
 
   return (
     <>
-      <section
-        id="focus-objective"
-        className="w-full py-12 md:py-24 bg-zinc-700"
-      >
+      <section id="transparency" className="w-full py-12 md:py-24 bg-zinc-700">
         <Container>
-          <div className="grid grid-cols-1 md:grid-cols-[1fr_480px] gap-8">
+          <section className="grid grid-cols-1 md:grid-cols-[1fr_480px] gap-8">
             <div className="space-y-8">
               <header>
-                <Typography as="pre-title">Como trabalhamos</Typography>
-                <Typography as="h2">Foco no Objetivo</Typography>
+                <Typography as="h2">Transparência nos Processos</Typography>
                 <Typography as="subtitle">
-                  Trabalhamos lado a lado com você para entender suas
-                  necessidades e objetivos, garantindo que cada solução entregue
-                  seja perfeitamente alinhada às suas expectativas.
+                  Cada etapa do trabalho é compartilhada com o cliente,
+                  garantindo clareza e confiança.
                 </Typography>
               </header>
 
               <section className="space-y-4">
                 <div>
-                  <Typography as="h3">Abordagem Centrada no Cliente</Typography>
+                  <Typography as="h3">Comunicação aberta</Typography>
                   <Typography as="p">
-                    Dedicamos tempo para entender profundamente seu negócio,
-                    seus desafios e seus objetivos, para que possamos fornecer
-                    soluções que realmente atendam às suas necessidades.
+                    Acreditamos na comunicação transparente e aberta com nossos
+                    clientes. Você sempre saberá o que está acontecendo com seu
+                    projeto.
                   </Typography>
                 </div>
 
                 <div>
-                  <Typography as="h3">Soluções sob medida</Typography>
+                  <Typography as="h3">Abordagem colaborativa</Typography>
                   <Typography as="p">
-                    Nossa equipe cria soluções personalizadas que estão
-                    perfeitamente alinhadas com seus objetivos, garantindo que
-                    você obtenha o máximo valor de nossa parceria.
+                    Nossa equipe trabalha em estreita colaboração com você para
+                    garantir que suas necessidades e objetivos sejam atendidos.
+                    Estamos aqui para apoiá-lo em cada etapa do caminho.
                   </Typography>
                 </div>
 
                 <div>
-                  <Typography as="h3">Colaboração contínua</Typography>
+                  <Typography as="h3">Relatórios detalhados</Typography>
                   <Typography as="p">
-                    Trabalhamos em estreita colaboração com você durante todo o
-                    ciclo de vida do projeto para garantir que estejamos sempre
-                    no caminho certo e entregando os resultados que você
-                    precisa.
+                    Fornecemos relatórios de progresso detalhados e
+                    atualizações, para que você sempre saiba o status do seu
+                    projeto.
                   </Typography>
                 </div>
               </section>
             </div>
 
-            <div className="hidden md:block overflow-hidden rounded-3xl">
-              <img
-                src="/assets/how-we-work-1.svg"
-                className="object-contain w-full h-full"
+            <div className="hidden max-h-[500px] md:block overflow-hidden rounded-3xl">
+              <Image
+                src="/assets/how-we-work-3.svg"
                 alt=""
+                width={480}
+                height={480}
+                className="object-contain w-full h-full"
               />
             </div>
-          </div>
+          </section>
         </Container>
       </section>
 
@@ -193,58 +187,62 @@ export const HowWeWork = () => {
         </Container>
       </section>
 
-      <section id="transparency" className="w-full py-12 md:py-24 bg-zinc-700">
+      <section
+        id="focus-objective"
+        className="w-full py-12 md:py-24 bg-zinc-700"
+      >
         <Container>
-          <section className="grid grid-cols-1 md:grid-cols-[1fr_480px] gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-[1fr_480px] gap-8">
             <div className="space-y-8">
               <header>
-                <Typography as="h2">Transparência nos Processos</Typography>
+                <Typography as="pre-title">Como trabalhamos</Typography>
+                <Typography as="h2">Foco no Objetivo</Typography>
                 <Typography as="subtitle">
-                  Cada etapa do trabalho é compartilhada com o cliente,
-                  garantindo clareza e confiança.
+                  Trabalhamos lado a lado com você para entender suas
+                  necessidades e objetivos, garantindo que cada solução entregue
+                  seja perfeitamente alinhada às suas expectativas.
                 </Typography>
               </header>
 
               <section className="space-y-4">
                 <div>
-                  <Typography as="h3">Comunicação aberta</Typography>
+                  <Typography as="h3">Abordagem Centrada no Cliente</Typography>
                   <Typography as="p">
-                    Acreditamos na comunicação transparente e aberta com nossos
-                    clientes. Você sempre saberá o que está acontecendo com seu
-                    projeto.
+                    Dedicamos tempo para entender profundamente seu negócio,
+                    seus desafios e seus objetivos, para que possamos fornecer
+                    soluções que realmente atendam às suas necessidades.
                   </Typography>
                 </div>
 
                 <div>
-                  <Typography as="h3">Abordagem colaborativa</Typography>
+                  <Typography as="h3">Soluções sob medida</Typography>
                   <Typography as="p">
-                    Nossa equipe trabalha em estreita colaboração com você para
-                    garantir que suas necessidades e objetivos sejam atendidos.
-                    Estamos aqui para apoiá-lo em cada etapa do caminho.
+                    Nossa equipe cria soluções personalizadas que estão
+                    perfeitamente alinhadas com seus objetivos, garantindo que
+                    você obtenha o máximo valor de nossa parceria.
                   </Typography>
                 </div>
 
                 <div>
-                  <Typography as="h3">Relatórios detalhados</Typography>
+                  <Typography as="h3">Colaboração contínua</Typography>
                   <Typography as="p">
-                    Fornecemos relatórios de progresso detalhados e
-                    atualizações, para que você sempre saiba o status do seu
-                    projeto.
+                    Trabalhamos em estreita colaboração com você durante todo o
+                    ciclo de vida do projeto para garantir que estejamos sempre
+                    no caminho certo e entregando os resultados que você
+                    precisa.
                   </Typography>
                 </div>
               </section>
             </div>
 
-            <div className="hidden max-h-[500px] md:block overflow-hidden rounded-3xl">
-              <Image
-                src="/assets/how-we-work-3.svg"
-                alt=""
-                width={480}
-                height={480}
+            <div className="hidden md:block overflow-hidden rounded-3xl">
+              <img
+                src="/assets/how-we-work-1.svg"
                 className="object-contain w-full h-full"
+                alt=""
               />
             </div>
-          </section>
+          </div>
         </Container>
       </section>
     </>
